79f7a441ffa0247e89e25780efb549d3ca39a2ba
Wosen <wosen.yu72@gmail.com>
2021-09-18 23:27:38 -0400

    random

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const engine= require('ejs-mate');
+const engine = require('ejs-mate');
 const methodOverride = require('method-override')
 
 const Item = require('./models/item');
@@ -21,63 +21,63 @@ app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', engine)
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 
 // show all items
-app.get('/items', async(req,res) =>{
+app.get('/items', async (req, res) => {
 
     const items = await Item.find({})
-    
-    res.render('index',{items})
+
+    res.render('index', { items })
 })
 
 // show new item
-app.get('/items/new',(req,res) => {
+app.get('/items/new', (req, res) => {
     res.render('new')
 })
 
 // create new item
-app.post('/items', async (req,res) => {
+app.post('/items', async (req, res) => {
     const newItem = new Item(req.body)
     await newItem.save();
     res.redirect('/items')
 })
 
 //find items by id
-app.get('/items/:id', async(req,res) =>{
+app.get('/items/:id', async (req, res) => {
 
     const { id } = req.params;
     const item = await Item.findById(id)
-    
-    res.render('show',{item})
+
+    res.render('show', { item })
 })
 
 // route for editing
-app.get('/items/:id/edit', async (req,res) => {
+app.get('/items/:id/edit', async (req, res) => {
     const { id } = req.params;
     const item = await Item.findById(id)
 
-    res.render('edit', {item})
+    res.render('edit', { item })
 })
 
 //update items 
-app.put('/items/:id', async (req,res) => {
+app.put('/items/:id', async (req, res) => {
     const { id } = req.params;
-    const editItem = await Item.findByIdAndUpdate(id, req.body, {runValidators: true, new: true} )
+    const editItem = await Item.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
 
     res.redirect(`/items/${editItem._id}`)
 })
 
 //delete item
-app.delete('/items/:id', async (req,res) => {
+app.delete('/items/:id', async (req, res) => {
     const { id } = req.params;
     const deleteItem = await Item.findByIdAndDelete(id)
 
     res.redirect(`/items`)
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("APP IS ON PORT 3000")
 })
